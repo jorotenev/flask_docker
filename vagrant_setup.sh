98bf01416cc8bb07037cce273bb8b44272598281
Joro Tenev <chipsanbg@gmail.com>
2016-10-30 14:53:49 +0100

    update the versions of docker-engine and docker-compose

--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -1,12 +1,17 @@
 # download and install docker 
-wget -q -O - https://get.docker.io/gpg | apt-key add -
-echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
-apt-get update -qq; apt-get install -q -y --force-yes lxc-docker
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee /etc/apt/sources.list.d/docker.list
+sudo apt-get update
+sudo apt-get install docker-engine -y
+
+
 # Add vagrant user to the docker group
-usermod -a -G docker vagrant
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
 # install docker-compose
 # https://docs.docker.com/engine/installation/linux/ubuntulinux/
-curl -L https://github.com/docker/compose/releases/download/1.6.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # make it possible to connect from PyCharm to docker
@@ -21,7 +26,7 @@ fi
 
 
 # install heroku toolbelt
-wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+#wget -O- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 
 folder_to_use=$1
 if [[ $folder_to_use ]] ; then
@@ -36,3 +41,6 @@ echo "alias init_db='docker-compose run --no-deps --rm web python manage.py init
 echo "alias drop_db='docker-compose run --no-deps --rm web python manage.py drop_db'" >> '/home/vagrant/.bashrc'
 echo "alias drop_and_init_db='docker-compose run --no-deps --rm web python manage.py drop_and_init_db'" >> '/home/vagrant/.bashrc'
 echo "$1 added as default ssh init folder AND drop_and_init_db, init_db, drop_db and shell aliases added."
+
+
+echo "Do vagrant reload now"
